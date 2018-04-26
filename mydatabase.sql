d5af71ce98a606ea2ec4c8d1bd1906ed438e755b
Juan Gongora <gongora.animations@gmail.com>
2018-04-26 14:10:43 -0400

    Added initial exports of data base schema and data for version 1.3.1

---
+++ mydatabase.sql
@@ -0,0 +1,10 @@
+
+
+There is an update available!!!
+Install it to get the latest and greatest
+
+
+Updating helps us provide the best support
+https://github.com/lando/lando/releases/tag/v3.0.0-beta.40
+
+
